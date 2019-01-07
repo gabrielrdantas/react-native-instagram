b7eca67e1a6d69441f8d6ca908c1a51539fff494
Gabriel <gabriel.dantas@stormsec.com.br>
2019-01-07 17:56:11 -0200

    Incluing comments and change likes logic

--- src/presentation/home/HomeContainerComponent.js
+++ src/presentation/home/HomeContainerComponent.js
@@ -15,24 +15,63 @@ import HomePostComponent from './HomePostComponent';
 
 export default class HomeContainerComponent extends Component<> {
   render() {
-    const photos = [
-      {id: 1, usuario: 'Rafael'},
-      {id: 2, usuario: 'Alberto'},
-      {id: 3, usuario: 'Vitor'}
+    const dataAPI = [
+      {
+        id: 1,
+        userName: 'Rafael',
+        userProfileImage: require('../../assets/images/leao.jpg'),
+        photo: require('../../assets/images/leao.jpg'),
+        likesUsers:[2, 3],
+        legend: 'Leão boladão',
+        comments:[
+          {
+            userId: 1,
+            userName: 'Alberto',
+            comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+          },
+          {
+            userId: 3,
+            userName: 'Vitor',
+            comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+          }
+        ]
+      },
+      {
+        id: 2,
+        userName: 'Alberto',
+        userProfileImage: require('../../assets/images/leao.jpg'),
+        photo: require('../../assets/images/leao.jpg'),
+        likesUsers: [1, 3],
+        legend: 'Leão boladão',
+        comments:[]
+      },
+      {
+        id: 3,
+        userName: 'Vitor',
+        userProfileImage: require('../../assets/images/leao.jpg'),
+        photo: require('../../assets/images/leao.jpg'),
+        likesUsers: [1, 2],
+        legend: 'Leão boladão',
+        comments:[]
+      }
     ];
 
     return (
 
       <FlatList 
         style={styles.container}
-        data={photos}
+        data={dataAPI}
         keyExtractor={item => `list-item-${item.id}`}
         renderItem={ ({item}) =>
           <HomePostComponent 
-            profileImage={require('../../assets/images/leao.jpg')}
-            userName={item.usuario}
-            photo={require('../../assets/images/leao.jpg')}
-            isLiked={true}
+            profileImage={item.userProfileImage}
+            userId={item.userId}
+            userName={item.userName}
+            photo={item.photo}
+            isLiked={item.isLiked}
+            likesUsers={item.likesUsers}
+            legend={item.legend}
+            comments={item.comments}
           />
         }
       />
--- src/presentation/home/HomePostComponent.js
+++ src/presentation/home/HomePostComponent.js
@@ -5,25 +5,56 @@ import {
   View,
   Image,
   Dimensions,
-  TouchableOpacity
+  TouchableOpacity,
+  TextInput
 } from 'react-native';
 
 const width = Dimensions.get('screen').width;
+const userID = 99;
 
 export default class HomePostComponent extends Component {
 
-  loadIcon(isLiked){
-    return isLiked ?  require('../../assets/images/s2-checked.png') : 
+  constructor(props) {
+    super(props);
+    this.state = {
+      likesUsers: props.likesUsers
+    }
+  }
+
+  loadIcon(likesUsers){
+    return likesUsers.indexOf(userID) > -1 ?  require('../../assets/images/s2-checked.png') : 
                       require('../../assets/images/s2.png')
   }
   
-  like(){
-    const updatedPhoto = {
-      ...this.state.photo,
-      isLiked: !this.state.foto.isLiked
+  setlikesPhoto(){
+    let newListlikesUsers = [];
+    if (this.state.likesUsers.indexOf(userID) === -1) {
+      newListlikesUsers = this.state.likesUsers.concat(userID)
+    } else {
+      newListlikesUsers = this.state.likesUsers.filter(userId => userId !== userID);
     }
-  
-    this.setState({photo: updatedPhoto});
+    this.setState({
+      likesUsers: newListlikesUsers
+    });
+  }
+
+  showLikes(likers) {
+    if (likers.length < 1) {
+      return;
+    }
+    return (<Text style={styles.likes}>{likers.length} {likers.length > 1 ? 'curtidas': 'curtida'}</Text>)
+  }
+
+  showLegend(legend) {
+    if(legend === '') {
+      return;
+    }
+    return (
+      <View style={styles.legend}>
+        <Text style={styles.titleLegend}>{this.props.userName}</Text>
+        <Text>{legend}</Text>
+      </View>
+    );
   }
 
   render() {
@@ -37,11 +68,23 @@ export default class HomePostComponent extends Component {
         </View>
         <Image source={this.props.photo} style={styles.photo} />
         <View style={styles.footer}>
-          <TouchableOpacity onPress={this.like.bind(this)}>
+          <TouchableOpacity onPress={() => this.setlikesPhoto()}>
             <Image 
               style={styles.likesButton}
-              source={this.loadIcon(this.props.isLiked)} />
+              source={this.loadIcon(this.state.likesUsers)} />
           </TouchableOpacity>
+          {this.showLikes(this.state.likesUsers)}
+          {this.showLegend(this.props.legend)}
+
+        {this.props.comments.map(item => 
+          <View style={styles.comment} key={item.userId}>
+            <Text style={styles.titleComment}>{item.userName}</Text>
+            <Text>{item.comment}</Text>
+          </View>
+        )}
+
+        <TextInput style={styles.input} placeholder="Adicione um comentário..." />
+
         </View>
      </View>
      );
@@ -63,11 +106,36 @@ const styles = StyleSheet.create({
     width:width, 
     height:width
   },
+  likes: {
+    fontWeight: 'bold'
+  },
+  botaoDeLike: {
+    marginBottom: 10,
+    width: 40,
+    height: 40
+  },
   likesButton: {
     width: 40,
     height: 40
   },
   footer: {
     margin: 10
+  },
+  legend: {
+    flexDirection: 'row'
+  },
+  titleLegend: {
+    fontWeight: 'bold',
+    marginRight: 5
+  },
+  input: {
+    height: 40
+  },
+  comment: {
+    flexDirection: 'row'
+  },
+  titleComment: {
+    fontWeight: 'bold',
+    marginRight: 5
   }
 })
